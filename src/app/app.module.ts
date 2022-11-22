9739c07b86d13850b32ae86b034843f449f30110
Pablo Gatica <ga77ica@hotmail.com>
2022-11-22 08:03:25 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProvinciasComponent } from './components/provincias/provincias.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvinciasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
