2160df595c1ff32c531bf86e9f7970f1ccb9525f
PMartinGatica <pablomartin.gatica@gmail.com>
2022-11-25 14:48:52 -0300

    ProvinciaComponentSelec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProvinciasComponent } from './components/provincias/provincias.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProvinciasComponent,
-    FooterComponent
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
