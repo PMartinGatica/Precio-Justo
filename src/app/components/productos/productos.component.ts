023a1d881b7bd93f8c0db68235d3256804232fad
PMartinGatica <pablomartin.gatica@gmail.com>
2022-11-28 14:54:17 -0300

    ComponenteProducto

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ProvinciasService } from 'src/app/services/provincias.service';
+import { ProductosService } from 'src/app/services/productos.service';
+
+export interface Producto{
+  ean:number;
+  nombre:number;
+  precio:number;
+}
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit {
+  productos: Producto[] =[];
+
+  constructor(private productosSrv : ProductosService){
+    productosSrv.getProductos().subscribe((data:any)=>{
+      this.productos = data;
+    })
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 
 }
